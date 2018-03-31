a2c1d5add3a85fe9c8e1721a7f92de45d131e137
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-31 12:13:55 -0500

    Fixed AppComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpStatusModule } from './modules/http-status/http-status.module';
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent
             ],
+            imports: [
+                HttpStatusModule
+            ]
         }).compileComponents();
     }));
     it('should create the app', async(() => {
