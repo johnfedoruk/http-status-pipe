4e76fbb0749685d390d491c029384709fd42322f
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-31 11:59:04 -0500

    StatsPipe returns status code text when given a status code. TDD

--- src/app/modules/pipes/status.pipe.spec.ts
+++ src/app/modules/pipes/status.pipe.spec.ts
@@ -1,8 +1,176 @@
 import { StatusPipe } from './status.pipe';
 
+let pipe: StatusPipe;
+
+const statsMap = {
+    "100": "Continue",
+    "101": "Switching Protocols",
+    "102": "Processing",
+    "200": "OK",
+    "201": "Created",
+    "202": "Accepted",
+    "203": "Non-authoritative Information",
+    "204": "No Content",
+    "205": "Reset Content",
+    "206": "Partial Content",
+    "207": "Multi-Status",
+    "208": "Already Reported",
+    "226": "IM Used",
+    "300": "Multiple Choices",
+    "301": "Moved Permanently",
+    "302": "Found",
+    "303": "See Other",
+    "304": "Not Modified",
+    "305": "Use Proxy",
+    "307": "Temporary Redirect",
+    "308": "Permanent Redirect",
+    "400": "Bad Request",
+    "401": "Unauthorized",
+    "402": "Payment Required",
+    "403": "Forbidden",
+    "404": "Not Found",
+    "405": "Method Not Allowed",
+    "406": "Not Acceptable",
+    "407": "Proxy Authentication Required",
+    "408": "Request Timeout",
+    "409": "Conflict",
+    "410": "Gone",
+    "411": "Length Required",
+    "412": "Precondition Failed",
+    "413": "Payload Too Large",
+    "414": "Request-URI Too Long",
+    "415": "Unsupported Media Type",
+    "416": "Requested Range Not Satisfiable",
+    "417": "Expectation Failed",
+    "418": "I'm a teapot",
+    "421": "Misdirected Request",
+    "422": "Unprocessable Entity",
+    "423": "Locked",
+    "424": "Failed Dependency",
+    "426": "Upgrade Required",
+    "428": "Precondition Required",
+    "429": "Too Many Requests",
+    "431": "Request Header Fields Too Large",
+    "444": "Connection Closed Without Response",
+    "451": "Unavailable For Legal Reasons",
+    "499": "Client Closed Request",
+    "500": "Internal Server Error",
+    "501": "Not Implemented",
+    "502": "Bad Gateway",
+    "503": "Service Unavailable",
+    "504": "Gateway Timeout",
+    "505": "HTTP Version Not Supported",
+    "506": "Variant Also Negotiates",
+    "507": "Insufficient Storage",
+    "508": "Loop Detected",
+    "510": "Not Extended",
+    "511": "Network Authentication Required",
+    "599": "Network Connect Timeout Error"
+};
+
 describe('StatusPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatusPipe();
-    expect(pipe).toBeTruthy();
-  });
+    beforeEach(() => {
+        pipe = new StatusPipe();
+    });
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+    describe('toInt', () => {
+        it('should return an int when passed an int of type number', () => {
+            const num: number = 20;
+            expect(pipe['toInt'](num)).toEqual(num);
+        });
+        it('should return an int when passed an int of type string', () => {
+            const num: number = 20;
+            const str: string = `${num}`;
+            expect(pipe['toInt'](str)).toEqual(num);
+        });
+        it('should return NaN if passed a float of type number', () => {
+            const num: number = 20.1;
+            const str: string = `${num}`;
+            expect(()=>pipe['toInt'](str)).toThrow();
+        });
+        it('should return NaN if passed a float of type string', () => {
+            const num: number = 20.1;
+            const str: string = `${num}`;
+            expect(()=>pipe['toInt'](str)).toThrow();
+        });
+        it('should return NaN if passed a string that is not a number', () => {
+            const str: string = "Hello";
+            expect(()=>pipe["toInt"](str)).toThrow();
+        });
+        it('should return NaN if passed an object', () => {
+            const obj: any = {};
+            expect(()=>pipe["toInt"](obj)).toThrow();
+        });
+        it('should return NaN if passed an object', () => {
+            const obj: any = {};
+            expect(()=>pipe["toInt"](obj)).toThrow();
+        });
+        it('should return NaN if passed an array', () => {
+            const arr: any = [];
+            expect(()=>pipe["toInt"](arr)).toThrow();
+        });
+        it('should return NaN if passed a null', () => {
+            expect(()=>pipe["toInt"](null)).toThrow();
+        });
+        it('should return NaN if passed an undefined', () => {
+            expect(()=>pipe["toInt"](undefined)).toThrow();
+        });
+        it('should return NaN if passed a NaN', () => {
+            expect(()=>pipe["toInt"](NaN)).toThrow();
+        });
+    });
+    describe('onError',()=>{
+        it('should throw an error',()=>{
+            const msg: string = "Some Error Message";
+            expect(()=>pipe['onError'](msg)).toThrowError(msg);
+        });
+        it('should be overridable',(done)=>{
+            const msg: string = "Some Error Message";
+            class NoErrorPipe extends StatusPipe {
+                public onError(arg: string): void {
+                    expect(arg).toEqual(msg);
+                    done();
+                }
+            }
+            pipe = new NoErrorPipe();
+            pipe['onError'](msg)
+        });
+    });
+    describe('getStatusCodeText', () => {
+
+        Object.keys(statsMap).forEach((key)=>{
+            it(`should return text "${statsMap[key]}" for code ${key}`,()=>{
+                const num: number = pipe["toInt"](key);
+                const expected: string = statsMap[key];
+                expect(pipe["getStatusCodeText"](num)).toEqual(expected);
+            });
+        });
+
+        it(`should throw an error if passed an unknown status code`,()=>{
+            expect(()=>pipe["getStatusCodeText"](10000)).toThrow();
+        });
+
+    });
+
+    describe('transform', () => {
+
+        Object.keys(statsMap).forEach((key)=>{
+            it(`should return text "${statsMap[key]}" for code ${key}`,()=>{
+                const num: number = pipe["toInt"](key);
+                const expected: string = statsMap[key];
+                expect(pipe["transform"](num)).toEqual(expected);
+            });
+        });
+
+        Object.keys(statsMap).forEach((key)=>{
+            it(`should return text "${statsMap[key]}" for code "${key}"`,()=>{
+                const expected: string = statsMap[key];
+                expect(pipe["transform"](key)).toEqual(expected);
+            });
+        });
+
+    });
+
 });
--- src/app/modules/pipes/status.pipe.ts
+++ src/app/modules/pipes/status.pipe.ts
@@ -1,12 +1,100 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'status'
+    name: 'status'
 })
 export class StatusPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+    public transform(code: string | number): string | never {
+        return this.getStatusCodeText(this.toInt(code));
+    }
 
+    protected toInt(code: string | number): number {
+        let num: number;
+        if (typeof (code) === "number") {
+            num = code;
+        } else if (typeof (code) === "string") {
+            num = +code;
+        } else {
+            this.onError(`Status code ${code} cannot be converted to a number`);
+        }
+        if (Number.isInteger(num)) {
+            return num;
+        } else {
+            this.onError(`Status code ${code} is not an integer`);
+        }
+    }
+
+    protected getStatusCodeText(code: number): string {
+        switch(code) {
+            case 100: return "Continue";
+            case 101: return "Switching Protocols";
+            case 102: return "Processing";
+            case 200: return "OK";
+            case 201: return "Created";
+            case 202: return "Accepted";
+            case 203: return "Non-authoritative Information";
+            case 204: return "No Content";
+            case 205: return "Reset Content";
+            case 206: return "Partial Content";
+            case 207: return "Multi-Status";
+            case 208: return "Already Reported";
+            case 226: return "IM Used";
+            case 300: return "Multiple Choices";
+            case 301: return "Moved Permanently";
+            case 302: return "Found";
+            case 303: return "See Other";
+            case 304: return "Not Modified";
+            case 305: return "Use Proxy";
+            case 307: return "Temporary Redirect";
+            case 308: return "Permanent Redirect";
+            case 400: return "Bad Request";
+            case 401: return "Unauthorized";
+            case 402: return "Payment Required";
+            case 403: return "Forbidden";
+            case 404: return "Not Found";
+            case 405: return "Method Not Allowed";
+            case 406: return "Not Acceptable";
+            case 407: return "Proxy Authentication Required";
+            case 408: return "Request Timeout";
+            case 409: return "Conflict";
+            case 410: return "Gone";
+            case 411: return "Length Required";
+            case 412: return "Precondition Failed";
+            case 413: return "Payload Too Large";
+            case 414: return "Request-URI Too Long";
+            case 415: return "Unsupported Media Type";
+            case 416: return "Requested Range Not Satisfiable";
+            case 417: return "Expectation Failed";
+            case 418: return "I'm a teapot";
+            case 421: return "Misdirected Request";
+            case 422: return "Unprocessable Entity";
+            case 423: return "Locked";
+            case 424: return "Failed Dependency";
+            case 426: return "Upgrade Required";
+            case 428: return "Precondition Required";
+            case 429: return "Too Many Requests";
+            case 431: return "Request Header Fields Too Large";
+            case 444: return "Connection Closed Without Response";
+            case 451: return "Unavailable For Legal Reasons";
+            case 499: return "Client Closed Request";
+            case 500: return "Internal Server Error";
+            case 501: return "Not Implemented";
+            case 502: return "Bad Gateway";
+            case 503: return "Service Unavailable";
+            case 504: return "Gateway Timeout";
+            case 505: return "HTTP Version Not Supported";
+            case 506: return "Variant Also Negotiates";
+            case 507: return "Insufficient Storage";
+            case 508: return "Loop Detected";
+            case 510: return "Not Extended";
+            case 511: return "Network Authentication Required";
+            case 599: return "Network Connect Timeout Error";
+            default: throw new Error(`Unknown status code ${code}`);
+        }
+    }
+
+    public onError(error: string): never | void {
+        throw new Error(error);
+    }
 }
