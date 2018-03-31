e5626ed595618aa8771b10d2d2bfc082e1cf32ca
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-03-31 11:08:06 -0500

    Added basic pipe StatusPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StatusPipe } from './modules/pipes/status.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusPipe
   ],
   imports: [
     BrowserModule
